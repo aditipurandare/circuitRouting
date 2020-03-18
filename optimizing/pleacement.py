39e66755e96d1cf0fad08c5c4c3b95d378f38143
Nicolas Agostini <n.b.agostini@gmail.com>
2020-03-18 01:12:28 -0400

    Add more functions to placement

--- optimizing/pleacement.py
+++ optimizing/pleacement.py
@@ -1,18 +1,39 @@
 
-def calculate_net_WA(net, cell_locations):
+def get_pins_from_cell(cell):
 
     return None
+
+
+def get_pins_from_cells(cells):
+
+    return {get_pins_from_cell(c) for c in cells}
+
+
+def calculate_net_pins_WA(net, pins_locations):
+
+    return None
+
+
+def calculate_net_WA(net, cell_locations):
+
+    calculate_net_pins_WA(net,cell_locations)
+
+    return None
+
 
 def calculate_density_penalty(cell_locations):
 
     return None
 
+
 def optimize_WA(nets, cell_locations):
 
     return None
 
+
 def main():
     print("Hello")
+
 
 if __name__ == "__main__":
     main()
