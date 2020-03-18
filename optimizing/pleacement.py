805a60bd7c1c80b98657cd9012489472bfe93d25
Nicolas Agostini <n.b.agostini@gmail.com>
2020-03-18 01:06:47 -0400

    Add placing structure

---
+++ optimizing/pleacement.py
@@ -0,0 +1,18 @@
+
+def calculate_net_WA(net, cell_locations):
+
+    return None
+
+def calculate_density_penalty(cell_locations):
+
+    return None
+
+def optimize_WA(nets, cell_locations):
+
+    return None
+
+def main():
+    print("Hello")
+
+if __name__ == "__main__":
+    main()
