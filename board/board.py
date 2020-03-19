ed34bb649e47337667ed8c4fc2e01bb3a7d784ef
Nicolas Agostini <n.b.agostini@gmail.com>
2020-03-18 21:17:34 -0400

    Create Board.py file

---
+++ board/board.py
@@ -0,0 +1,54 @@
+
+
+class Element:
+    def __init__(self, name):
+        self.name = name
+        self.element_type = None
+
+    def get_name(self):
+        print(self.name)
+
+    def get_type(self):
+        print(self.element_type)
+
+
+class Pin(Element):
+
+    def __init__(self, name, x_offset=0, y_offset=0):
+        Element.__init__(self, name)
+
+        self.x_offset = x_offset
+        self.y_offset = y_offset
+
+
+class Module(Element):
+
+    def __init__(self, name, x_pos=0, y_pos=0, width=10, height=10):
+        Element.__init__(self, name)
+        Element.element_type = 'Module'
+
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+        self.x_pos = x_pos
+        self.y_pos = y_pos
+
+        # Every module has at least one pin 
+        self.pin_count = 0
+        self.pins = []
+        self.pins.append(Pin(self.name+"{:02d}".format(self.pin_count)))
+        self.pin_count = 1
+
+
+class Net(Element):
+
+    def __init__(self, name):
+        Element.__init__(self, name)
+
+
+class Board(Element):
+
+    # List of nets
+    # List of Modules
+
+    def __init__(self, name, x_size=0, y_size=0):
+        Element.__init__(self, name)
